f8d8dd41c7e7bf89f40080698cc003ea88e5bc65
Eduardo123007 <168853432+Eduardo123007@users.noreply.github.com>
2025-12-17 20:52:52 -0300

    Create Estoque-pythom.py

---
+++ Estoque-pythom.py
@@ -0,0 +1,15 @@
+print(f"=====Estuque=====\n")
+
+nome_produto = input("Digite o nome do produto: ")
+
+preço_caixa = float(input("Digite o preço da caixa R$: "))
+
+unidade_caixa = int(input("Digite a quantidades de unidade por caixa: "))
+
+margem_lucro = float(input("Digite a margem de lucro (%): "))
+
+lucro = preço_caixa + ( preço_caixa * margem_lucro / 100)
+
+preço_unidade = lucro / unidade_caixa
+
+print("O preço de venda do produto:", nome_produto, "é de R$",preço_unidade)
